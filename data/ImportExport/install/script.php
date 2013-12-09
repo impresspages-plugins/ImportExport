73a58c936a0e49edddc394e76da1d6099fd22dc1
marijusimpresspages <marijus@impresspages.org>
2013-12-09 10:49:34 +0200

    Added install script

---
+++ data/ImportExport/install/script.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package   ImpressPages
+ * @copyright Copyright (C) 2009 JSC Apro media.
+ * @license   GNU/GPL, see ip_license.html
+ */
+namespace Modules\navigation\social_icons;
+
+if (!defined('CMS')) exit;
+
+class Install{
+
+    public function execute(){
+
+
+    }
+}
+
